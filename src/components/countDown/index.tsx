dcf54c2165f303a4217297faad6493e2bf02a2a5
layaCoder <412046213@qq.com>
2023-07-12 18:37:12 +0800

    feat: count down rotate demo

--- src/components/countDown/index.tsx
+++ src/components/countDown/index.tsx
@@ -5,10 +5,12 @@ import './index.less'
 const CountDown = () => {
 
     const [count,setCount] =useState(15*60*1000)
+    const [active,setActive] = useState(false)
 
     useEffect(()=>{
         const timer = setInterval(()=>{
             setCount(count-1000)
+            setActive(!active)
         },1000)
         return ()=>{
             clearInterval(timer)
@@ -20,7 +22,10 @@ const CountDown = () => {
         <div className='cd-wrapper'>
             <div>{hour}:</div>
             <div>{minute}:</div>
-            <div className='cd-second-item'>{second}</div>
+            <div className={`cd-second-wrapper ${active?'roll':''}`}>
+                <div className='cd-second-item front'>{second}</div>
+                <div className='cd-second-item back'>{second}</div>
+            </div>
         </div> )
     ;
 }
